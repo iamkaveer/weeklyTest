3ea7052814420346e9012df472ee6b3824e4c136
Kaveer <41718548+iamkaveer@users.noreply.github.com>
2023-04-16 14:23:26 +0530

    Update MailService.java

--- MailService.java
+++ MailService.java
@@ -1,11 +1,9 @@
 package com.example.Weekly_Test;
-
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Properties;
 
 @Service
@@ -13,11 +11,9 @@ public class MailService {
 
     private final String HOST = "smtp.gmail.com";
     private final String SENDER = "kabirhossain293@gmail.com";
-    private final String PASSWORD = "lfyrpzxfyabuwiho";
-    private final String RECEIVER = "iamkaveer1384@gmail.com"; // replace with the recipient's email address
-
+    private final String PASSWORD = " ";
+    private final String RECEIVER = "iamkaveer1384@gmail.com"
     private final Properties properties = new Properties();
-
     @Autowired
     public MailService() {
         properties.put("mail.smtp.host", HOST);
@@ -25,7 +21,6 @@ public class MailService {
         properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.auth", "true");
     }
-
     public void sendEmail(String subject, String body) throws MessagingException {
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
