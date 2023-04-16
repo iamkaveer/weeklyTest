ba570f3565a94797e6f17f01649f31d6b9597aae
Kaveer <41718548+iamkaveer@users.noreply.github.com>
2023-04-16 14:25:31 +0530

    Update CustomerController.java

--- controller/CustomerController.java
+++ controller/CustomerController.java
@@ -1,27 +1,22 @@
 package com.example.Weekly_Test.controller;
-
 import com.example.Weekly_Test.MailService;
 import com.example.Weekly_Test.Customer;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 @RestController
 public class CustomerController {
 
     @Autowired
     private Customer customer;
-
     @Autowired
     private MailService mailService;
-
     @GetMapping("/customer")
     public List<Customer> getCustomers() {
         return List.of(customer);
     }
-
     @GetMapping("/sendmail")
     public String sendMail() throws MessagingException {
         String objectDetails = customer.toString();
